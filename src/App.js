eb5b2f0f5b36038fd10430edbefe86b47ac742f6
hi33741 <hi33741@naver.com>
2021-08-22 00:13:56 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer'
-import Todos from './component/Todos';
+
 import TodosContainer from './containers/TodosContainer';
 
 const App = () => {

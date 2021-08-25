6ef75d5536980e2ddbf257807f6e6da2020e9518
hi33741 <hi33741@naver.com>
2021-08-25 23:59:34 +0900

    final

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,30 +1,33 @@
-import React,{useCallback} from 'react';
-import {useSelector,useDispatch} from 'react-redux'
-import todos, {changeInput, insert, toggle, remove} from '../modules/todos'
-import Todos from '../component/Todos'
-
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import todos, { changeInput, insert, toggle, remove } from '../modules/todos';
+import Todos from '../component/Todos';
 
 const TodosContainer = () => {
-    const {input,todos} = useSelector(({todos}) =>({
-        input: todos.input,
-        todos:todos.todos
-    }))
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
 
-    const dispatch = useDispatch()
-    const onChangeInput = useCallback(input=>dispatch(changeInput(input)),[dispatch])
-    const onInsert = useCallback(text=>dispatch(insert(text)),[dispatch])
-    const onToggle = useCallback(id=>dispatch(toggle(id)),[dispatch])
-    const onRemove = useCallback(id=>dispatch(remove(id)),[dispatch])
-    return ( //토도스에 보내주고있음
-        <Todos
-        input ={input}
-        todos ={todos}
-        onChangeInput={onChangeInput}
-        onInsert={onInsert}
-        onToggle={onToggle}
-        onRemove={onRemove}
-        />
-    );
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(
+    (input) => dispatch(changeInput(input)),
+    [dispatch],
+  );
+  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+  return (
+    //토도스에 보내주고있음
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  );
 };
 
-export default React.memo(TodosContainer)
+export default React.memo(TodosContainer);
